818e13520aa9fb5603ae2f7d661474cee91f785f
armaedagon <68027618+armaedagon@users.noreply.github.com>
2021-10-31 22:17:30 +0530

    feat: Added interview ques to swap first - last no

---
+++ swapFirstandLast.py
@@ -0,0 +1,14 @@
+def swapList(newList):
+    size = len(newList)
+     
+    # Swapping
+    temp = newList[0]
+    newList[0] = newList[size - 1]
+    newList[size - 1] = temp
+     
+    return newList
+     
+# Driver code
+newList = [12, 53, 89, 56, 24]
+ 
+print(swapList(newList))
