68102928771f7b9f994f12a2e234351449c2b6d8
armaedagon <68027618+armaedagon@users.noreply.github.com>
2021-10-31 22:22:50 +0530

    feat: Added find nth largest no

---
+++ find n largest no.py
@@ -0,0 +1,20 @@
+def Nmaxelements(list1, N):
+    final_list = []
+  
+    for i in range(0, N): 
+        max1 = 0
+          
+        for j in range(len(list1)):     
+            if list1[j] > max1:
+                max1 = list1[j];
+                  
+        list1.remove(max1);
+        final_list.append(max1)
+          
+    print(final_list)
+  
+# Driver code
+list1 = [2, 6, 41, 5, 0, 3, 7, 556, 10]
+N = 3
+
+Nmaxelements(list1, N)
